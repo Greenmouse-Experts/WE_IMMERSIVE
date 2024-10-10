ae64d08edfb20a59c1341309d9e135e33533a4e6
wills <godswillIjezei@gmail.com>
2024-10-10 17:02:21 +0100

    auth pages

--- src/modules/landing/robust-community/robust-grid.tsx
+++ src/modules/landing/robust-community/robust-grid.tsx
@@ -1,15 +1,14 @@
-
 const RobustGrid = () => {
   return (
     <div className="mt-16 lg:mt-32">
-        <div className="grid grid-cols-3 gap-6">
-            <div className="row-span-2 bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575678/WE%20Immersive/Group_1000005668_fwfmnp.png')] bg-fit"></div>
-            <div className="h-[350px] bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575679/WE%20Immersive/Group_1000005669_rocejk.png')] bg-fit"></div>
-            <div className="h-[350px] bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575680/WE%20Immersive/Group_1000005670_wzmizq.png')] bg-fit"></div>
-            <div className="col-span-2 h-[350px] bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575653/WE%20Immersive/Group_1000005674_1_s1pify.png')] bg-fit"></div>
-        </div>
+      <div className="grid grid-cols-3 gap-6">
+        <div className="row-span-2 bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575678/WE%20Immersive/Group_1000005668_fwfmnp.png')] bg-fit"></div>
+        <div className="h-[350px] bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575679/WE%20Immersive/Group_1000005669_rocejk.png')] bg-fit"></div>
+        <div className="h-[350px] bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575680/WE%20Immersive/Group_1000005670_wzmizq.png')] bg-fit"></div>
+        <div className="col-span-2 h-[350px] bg-[url('https://res.cloudinary.com/do2kojulq/image/upload/v1728575653/WE%20Immersive/Group_1000005674_1_s1pify.png')] bg-fit"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RobustGrid
+export default RobustGrid;
