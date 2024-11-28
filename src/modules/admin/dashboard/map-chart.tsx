74ed0aabd1d7972889c18aff60bcabcb7d9de3b6
mosesinawo <inawo.moses@gmail.com>
2024-11-28 16:46:31 +0100

    fix build

--- src/modules/admin/dashboard/map-chart.tsx
+++ src/modules/admin/dashboard/map-chart.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
-import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
+// import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 import { MdArrowOutward, MdOutlineArrowDropDown } from "react-icons/md";
 
