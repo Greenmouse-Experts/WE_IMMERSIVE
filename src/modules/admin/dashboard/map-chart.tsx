77eddf2537bab4bad77abe617a3d46000ac7c667
mosesinawo <inawo.moses@gmail.com>
2024-11-28 16:52:52 +0100

    map adjusted

--- src/modules/admin/dashboard/map-chart.tsx
+++ src/modules/admin/dashboard/map-chart.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
-// import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
+import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 import am5themes_Animated from "@amcharts/amcharts5/themes/Animated";
 import { MdArrowOutward, MdOutlineArrowDropDown } from "react-icons/md";
 
@@ -27,11 +27,14 @@ const MapChart = () => {
       })
     );
 
-    // let polygonSeries = chart.series.push(
-    //   am5map.MapPolygonSeries.new(root, {
-    //     geoJSON: am5geodata_worldLow,
-    //   })
-    // );
+
+    let polygonSeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_worldLow,
+      })
+    );
+
+    console.log(polygonSeries)
 
     let pointSeries = chart.series.push(
       am5map.MapPointSeries.new(root, {
