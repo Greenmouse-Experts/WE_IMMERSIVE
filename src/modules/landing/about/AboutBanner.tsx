66973a30b92366ed125dae4645f133d6a72449ac
Hamzat Abdulazeez <95409350+HamzatAbdulazeez@users.noreply.github.com>
2025-02-11 16:02:24 +0100

    final desktop version

--- src/modules/landing/about/AboutBanner.tsx
+++ src/modules/landing/about/AboutBanner.tsx
@@ -3,11 +3,11 @@ import globe from "../../../assets/globe.png";
 
 const AboutBanner = () => {
   return (
-    <div className=" bg-gradient-to-r from-[#ffffff] via-[#1D9CD7] to-[#6F0AFF]">
+    <div className="bg-gradient-to-r from-blue-100 to-purple-200">
       <div className="flex align-middle items-center">
         <div className="pl-[9%] py-[3%] w-[85%]">
             <p className="Mulish text-[14px] mb-3 font-[700]">ABOUT WE IMMERSIVE</p>
-            <h1 className="unbound text-[30px] font-[700] mb-4 leading-[1.6]">The creative hub built for today’s digital artists, designers, and creators.</h1>
+            <h1 className="unbound text-[30px] font-[700] mb-4 leading-[1.6] pr-[20%]">The creative hub built for today’s digital artists, designers, and creators.</h1>
             <p className="text-[14px] text-[#696767] w-[70%] mb-5">WE.IMMERSIVE leverages cutting-edge technology to empower African creators through an equitable marketplace and innovative payment systems.</p>
             <Button 
                 title={"Join Now"} 
--- src/modules/landing/about/Card1.tsx
+++ src/modules/landing/about/Card1.tsx
@@ -27,13 +27,13 @@ const Card1 = () => {
   return (
     <div className="flex justify-between mt-[10%]">
         {
-            cards.map((item, index) => (
-                <div key={index} className="flex flex-col rounded-[15px] w-[30%] text-white pb-5" style={{ backgroundColor: item.color }}>
-                    <img src={item.img} width="149px" alt="img" className={`relative left-[30%] ${index === 2 ? "-mt-[23%]" : "-mt-[18%]"}`}/>
-                    <p className="unbound mb-5 mt-8 text-[15px] font-[600]">{item.title}</p>
-                    <p className="text-[13px] px-[15%] font-[700]">{item.content}</p>
-                </div>
-            ))
+         cards.map((item, index) => (
+            <div key={index} className="flex flex-col rounded-[15px] w-[30%] text-white pb-5" style={{ backgroundColor: item.color }}>
+                <img src={item.img} width="149px" alt="img" className={`relative left-[30%] ${index === 2 ? "-mt-[19%]" : "-mt-[18%]"}`}/>
+                <p className="unbound mb-5 mt-8 text-[15px] font-[600]">{item.title}</p>
+                <p className="text-[13px] px-[15%] font-[700]">{item.content}</p>
+            </div>
+         ))
         }
     </div>
   )
