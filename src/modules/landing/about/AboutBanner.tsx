5206ef791dc37f4d53bcb6495305fbcca434be1c
Acha-Goodness <acharious1668@gmail.com>
2025-02-12 09:24:02 +0100

    about banner modification

--- src/modules/landing/about/AboutBanner.tsx
+++ src/modules/landing/about/AboutBanner.tsx
@@ -4,7 +4,7 @@ import globe from "../../../assets/globe.png";
 const AboutBanner = () => {
   return (
     <div className="bg-gradient-to-r from-blue-100 to-purple-200">
-      <div className="flex flex-col-reverse md:flex-row align-middle items-center py-11">
+      <div className="flex flex-col-reverse md:flex-row align-middle items-center py-11 md:py-0">
         <div className="md:pl-[9%] py-[3%] md:w-[85%]">
             <p className="Mulish text-[14px] text-center md:text-left mb-3 font-[700]">ABOUT WE IMMERSIVE</p>
             <h1 className="unbound md:text-[30px] text-center md:text-left font-[700] mb-4 leading-[1.6] md:pr-[20%]">The creative hub built for today’s digital artists, designers, and creators.</h1>
