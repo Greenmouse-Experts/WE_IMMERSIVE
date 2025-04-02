70b1484a3e7b15d5fd82d5cffa54bc9aea79aea8
mosesinawo <inawo.moses@gmail.com>
2025-04-02 08:44:39 +0100

    fix build

--- src/hooks/useVoiceChanger.tsx
+++ src/hooks/useVoiceChanger.tsx
@@ -1,21 +1,28 @@
 import { useState, useCallback } from "react";
 import { ElevenLabsClient, play } from "elevenlabs";
 
-const ELEVENLABS_API_KEY = "sk_ebe73bf0c9c006feffbf5b6529ecd309624e57de95b2a625";
+const ELEVENLABS_API_KEY =
+  "sk_ebe73bf0c9c006feffbf5b6529ecd309624e57de95b2a625";
 
 const useVoiceChanger = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [audioStream, setAudioStream] = useState<string | null>(null);
 
-  const convertSpeech = useCallback(async (voiceId:string, audioUrl:any) => {
+  const convertSpeech = useCallback(async (voiceId: string, audioUrl: any) => {
     setLoading(true);
     setError(null);
 
+    console.log(audioUrl);
+
     try {
-        const client = new ElevenLabsClient({ apiKey: ELEVENLABS_API_KEY });
-      const response = await fetch("https://res.cloudinary.com/do2kojulq/video/upload/v1743430600/cde6d18c-be31-4d7d-ae91-ab7e5b876f80_vl7urq.mp3");
-      const audioBlob = new Blob([await response.arrayBuffer()], { type: "audio/mp3" });
+      const client = new ElevenLabsClient({ apiKey: ELEVENLABS_API_KEY });
+      const response = await fetch(
+        "https://res.cloudinary.com/do2kojulq/video/upload/v1743430600/cde6d18c-be31-4d7d-ae91-ab7e5b876f80_vl7urq.mp3"
+      );
+      const audioBlob = new Blob([await response.arrayBuffer()], {
+        type: "audio/mp3",
+      });
 
       const stream = await client.speechToSpeech.convert(voiceId, {
         audio: audioBlob,
@@ -33,7 +40,7 @@ const useVoiceChanger = () => {
 
       setAudioStream(url);
       await play(stream);
-    } catch (err:any) {
+    } catch (err: any) {
       setError(err);
     } finally {
       setLoading(false);
