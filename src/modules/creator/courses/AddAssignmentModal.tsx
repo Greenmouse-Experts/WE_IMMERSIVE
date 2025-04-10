6e3445da550361b692279bd4d9b1f7f35c033487
mosesinawo <inawo.moses@gmail.com>
2025-04-10 22:26:22 +0100

    fix build 2

--- src/modules/creator/courses/AddAssignmentModal.tsx
+++ src/modules/creator/courses/AddAssignmentModal.tsx
@@ -6,7 +6,7 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
 const AssignmentCreator = ({
-  lessonQuizId,
+  // lessonQuizId,
   handleOpen,
 }: {
   lessonQuizId: string;
